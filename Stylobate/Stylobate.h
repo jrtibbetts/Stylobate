834e09b44861a72db9522ad966d3ab299659650e
Jason R Tibbetts <jason@tibbetts.net>
2018-05-06 15:50:51 -0400

    First version.

---
+++ Stylobate/Stylobate.h
@@ -0,0 +1,19 @@
+//
+//  Stylobate.h
+//  Stylobate
+//
+//  Created by Jason R Tibbetts on 5/6/18.
+//  Copyright © 2018 Poikile Creations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Stylobate.
+FOUNDATION_EXPORT double StylobateVersionNumber;
+
+//! Project version string for Stylobate.
+FOUNDATION_EXPORT const unsigned char StylobateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stylobate/PublicHeader.h>
+
+
