eeb2be8c7809219e28781e161cf26c75b9341fdf
Jason R Tibbetts <jason@tibbetts.net>
2019-05-05 19:33:48 -0400

    No ticket: Added tests for NSManagedObjectContext.fetchOrCreate(withRequest:updateWith:).

--- Source/Core Data/NSManagedObjectContext+ObjectCreator.swift
+++ Source/Core Data/NSManagedObjectContext+ObjectCreator.swift
@@ -5,21 +5,33 @@ import Foundation
 
 public extension NSManagedObjectContext {
 
-    /// Fetch (using a specified request) or create (using an initialization
-    /// block) an `NSManagedObject` subtype.
-    ///
-    /// - parameter request: The fetch request. This should already have its
-    ///   predicate, sort order, and other properties set before it's passed
-    ///   in.
-    /// - parameter initialize: A block that's called to create a new managed
-    ///   object if no matching one was found.
     func fetchOrCreate<T: NSManagedObject>(with request: NSFetchRequest<T>,
                                            initialize: (NSManagedObjectContext) -> T) throws -> T {
         // Fetch.................................or create.
         return try fetch(request).first ?? initialize(self)
     }
 
-    func fetchOrCreate<T: NSManagedObject>(with request: NSFetchRequest<T>,
+    /// Fetch the first existing object that matches the request, or create a
+    /// new one, then update the object with the block that's passed in. Note
+    /// that this will apply the block _even if_ the object already existed.
+    /// Call `hasChanges` on the returned object to see whether the update
+    /// block did anything meaningful.
+    ///
+    /// - parameter request: The fetch request. This should be constructed in
+    ///             such a way that a single object (or `nil`) is returned,
+    ///             because only the first matched object will be updated and
+    ///             returned.
+    /// - parameter update: The block to apply to the fetched or created
+    ///             managed object. This will be applied to _every_ object,
+    ///             even if it already exists, so it shouldn't be overly
+    ///             complex. If you don't want the update to be applied to
+    ///             everything, call
+    ///             `NSManagedObjectContext.fetchOrCreate(with:initializer:)`
+    ///             instead.
+    ///
+    /// - returns:  An existing or new instance of `T`, after it's been passed
+    ///             into the `update` block.
+    func fetchOrCreate<T: NSManagedObject>(withRequest request: NSFetchRequest<T>,
                                            updateWith update: (T) -> Void) throws -> T {
         // Fetch.................................or create.
         let object = try fetch(request).first ?? T.init(context: self)
--- Tests/Core Data/NSManagedObjectContext+ObjectCreatorTests.swift
+++ Tests/Core Data/NSManagedObjectContext+ObjectCreatorTests.swift
@@ -7,6 +7,7 @@ import XCTest
 class NSManagedObjectContext_FetchOrCreate: FetchingTestBase {
 
     // MARK: - fetchOrCreate(with:initialize:)
+
     func testFetchOrCreateTwiceReturnsSameValue() throws {
         let request = Person.fetchRequest(sortDescriptors: [sortByName],
                                           predicate: NSPredicate(format: "name == \"Frank Abberline\""))
@@ -28,6 +29,40 @@ class NSManagedObjectContext_FetchOrCreate: FetchingTestBase {
 
         XCTAssertFalse(person1 === person2)
     }
+
+    // MARK: - fetchOrCreate(withRequest:updateWith:)
+
+    func testFetchOrCreateUpdateWithTwiceReturnsSameValue() throws {
+        let request: NSFetchRequest<Person> = Person.fetchRequest(sortDescriptors: [sortByName],
+                                                                  predicate: NSPredicate(format: "name == \"Frank Abberline\""))
+
+        let person1 = try testingContext.fetchOrCreate(withRequest: request) { (person) in
+            person.name = "Frank Abberline"
+            person.sortName = "Abberline, Frank"
+        }
+        let person2 = try testingContext.fetchOrCreate(withRequest: request) { (person) in
+            person.name = "Frank Abberline"
+            person.sortName = "Abberline, Frank"
+        }
+        XCTAssertTrue(person1 === person2)
+    }
+
+    func testFetchOrCreateUpdateWithDifferentPredicatesReturnsDifferentValues() throws {
+        let request: NSFetchRequest<Person> = Person.fetchRequest(sortDescriptors: [sortByName],
+                                                                  predicate: NSPredicate(format: "name == \"Sir William Gull\""))
+        let person1 = try testingContext.fetchOrCreate(withRequest: request) { (person) in
+            person.name = "Frank Abberline"
+            person.sortName = "Abberline, Frank"
+        }
+        let person2 = try testingContext.fetchOrCreate(withRequest: request) { (person) in
+            person.name = "Frank Abberline"
+            person.sortName = "Abberline, Frank"
+        }
+
+        XCTAssertFalse(person1 === person2)
+    }
+
+    // MARK: - Utilities
 
     let sortByName = (\Person.name).sortDescriptor()
 
