e25df7f9290fb7e6e82a831b2825cf02c121d324
Jason R Tibbetts <jason@tibbetts.net>
2019-06-08 23:34:55 -0400

    Added a Swift Package Manager file.

---
+++ Package@swift-4.2.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let pkg = Package(name: "Stylobate")
+pkg.products = [
+    .library(name: "Stylobate", targets: ["Stylobate"]),
+]
+
+let stylobate: Target = .target(name: "Stylobate")
+stylobate.path = "Sources"
+stylobate.exclude = [
+]
+pkg.swiftLanguageVersions = [.v4, .v4_2]
+pkg.targets = [
+    stylobate,
+    .testTarget(name: "StylobateTests", dependencies: ["Stylobate"]), 
