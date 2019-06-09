f457a6062c9492eaff415b508884eac12ac74bf7
Jason R Tibbetts <jason@tibbetts.net>
2019-06-09 11:09:10 -0400

    Added Swift 5 to Package.swift and fixed another syntax error.

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let pkg = Package(name: "Stylobate")
 pkg.products = [
-    .library(name: "Stylobate", targets: ["Stylobate"]),
+    .library(name: "Stylobate", targets: ["Stylobate"])
 ]
 
 let stylobate: Target = .target(name: "Stylobate")
 stylobate.path = "Source"
 stylobate.exclude = [
 ]
-pkg.swiftLanguageVersions = [.v4, .v4_2]
+pkg.swiftLanguageVersions = [.v4, .v4_2, .v5]
 pkg.targets = [
     stylobate,
     .testTarget(name: "StylobateTests", dependencies: ["Stylobate"])
