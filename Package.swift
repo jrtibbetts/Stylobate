faf0856d8df18420129fdd3686e26f20997c602a
Jason R Tibbetts <jason@tibbetts.net>
2019-12-21 19:19:59 -0500

    Reformatted the Package.swift file. It says that '5' isn't a valid Swift version number. Then what is?

--- Package.swift
+++ Package.swift
@@ -1,18 +1,33 @@
 // swift-tools-version:5.1
 import PackageDescription
 
-let pkg = Package(name: "Stylobate")
-pkg.products = [
-    .library(name: "Stylobate", targets: ["Stylobate"])
-]
+let pkg = Package(
+  name: "Stylobate",
 
-let stylobate: Target = .target(name: "Stylobate")
-stylobate.path = "Source"
-stylobate.exclude = [
-]
-pkg.swiftLanguageVersions = [.v4, .v4_2, .v5]
-pkg.targets = [
-    stylobate,
-    .testTarget(name: "StylobateTests", dependencies: ["Stylobate"])
+  swiftLanguageVersions = [.v4, .v4_2, .v5],
+
+  products: [
+    .library(
+      name: "Stylobate",
+      targets: ["Stylobate"]
+    )
+  ],
+
+  dependencies: [ ],
+
+  targets: [
+    .target(
+      name: "Stylobate",
+      dependencies: ["UIKit"],
+      path: "Source",
+      exclude = [ ]
+    ),
+    .testTarget(
+      name: "StylobateTests",
+      dependencies: ["Stylobate"],
+      path: "Tests",
+      exclude = [ ]
+    )
+  ]
 ]
 
