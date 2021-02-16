c940b97a0477387a8053a31919edea5d065a72f3
Jason R Tibbetts <jason@tibbetts.net>
2021-02-15 20:30:09 -0500

    #15: [fix] Change Stylobate.resourceBundle and StylobateTests.resourceBundle to use the non-Swift Package Manager style of resource bundles.

--- Source/Stylobate.swift
+++ Source/Stylobate.swift
@@ -5,20 +5,22 @@ import Foundation
 public class Stylobate {
 
     public static var resourceBundle: Bundle = {
-        let bundleName = "Stylobate_Stylobate.bundle"
-
-        let locationCandidates = [Bundle.main.resourceURL,
-                                  Bundle(for: Stylobate.self).resourceURL,
-                                  Bundle.main.bundleURL]
-
-        for path in locationCandidates {
-            if let bundleUrl = path?.appendingPathComponent(bundleName),
-               let bundle = Bundle(url: bundleUrl) {
-                return bundle
-            }
-        }
-
-        fatalError("Failed to load the resource bundle from \(locationCandidates)")
+        return Bundle(for: Stylobate.self)
+// For whenever Swift Package Manager unfucks resources in bundles
+//        let bundleName = "Stylobate_Stylobate.bundle"
+//
+//        let locationCandidates = [Bundle.main.resourceURL,
+//                                  Bundle(for: Stylobate.self).resourceURL,
+//                                  Bundle.main.bundleURL]
+//
+//        for path in locationCandidates {
+//            if let bundleUrl = path?.appendingPathComponent(bundleName),
+//               let bundle = Bundle(url: bundleUrl) {
+//                return bundle
+//            }
+//        }
+//
+//        fatalError("Failed to load the resource bundle from \(locationCandidates)")
     }()
 
 }
