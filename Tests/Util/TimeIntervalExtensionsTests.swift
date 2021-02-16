45402dca4c812b1441b0ce09f0c23c938b918f13
Jason R Tibbetts <jason@tibbetts.net>
2021-02-15 20:25:38 -0500

    #15: [fix] Swift Package Manager simply isn't ready for prime-time. Go back to using the project file.

--- Tests/Util/TimeIntervalExtensionsTests.swift
+++ Tests/Util/TimeIntervalExtensionsTests.swift
@@ -8,9 +8,9 @@ class TimeIntervalExtensionsTests: XCTestCase {
     func test14DaysIs2Weeks() {
         XCTAssertEqual(14.days, 2.weeks)
     }
-    
+
     func test2YearsIs104WeeksAndTwoDays() {
         XCTAssertEqual(2.years, 104.weeks + 2.days)
     }
-    
+
 }
