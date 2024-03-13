5c62a6879ac6cace4d8cd5bad17931d19f74d1a2
Jason R Tibbetts <jason@tibbetts.net>
2024-03-13 01:20:06 -0400

    Renamed the Views & Displays folder to just Views, since the GitHub xcodebuild command couldn't handle the '&'.

--- Package.swift
+++ Package.swift
@@ -37,10 +37,10 @@ let pkg = Package(
                 .copy("JSON/SampleFoo.json"),
                 .process("Controllers/CollectionAndTableTests.storyboard"),
                 .process("Core Data/StylobateTestModel.xcdatamodeld"),
-                .process("Views & Displays/CollectionAndTableDisplayTests.xib"),
-                .process("Views & Displays/FormattedLabelTests.storyboard"),
-                .process("Views & Displays/MissingCellTests.xib"),
-                .process("Views & Displays/VideoPreview.xib")
+                .process("Views/CollectionAndTableDisplayTests.xib"),
+                .process("Views/FormattedLabelTests.storyboard"),
+                .process("Views/MissingCellTests.xib"),
+                .process("Views/VideoPreview.xib")
             ]
         )
     ]
